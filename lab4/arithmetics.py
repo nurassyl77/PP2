0b145a54ac693b147901bfc1e1d511df89dafcc2
nurassyl77 <nurasylmustafaev@gmail.com>
2025-02-27 22:39:33 +0500

    lab4

---
+++ lab4/arithmetics.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return abs(a - b)
+def divide(a, b):
+    if a > b and b != 0:
+        return a // b
+    elif b > a and a != 0:
+        return b // a
+def multiply(a, b):
+    return a * b
+def mod(a, b):
+    return a % b
